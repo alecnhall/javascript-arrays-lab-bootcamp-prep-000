615f081a024f3e8c41becc74009a82ec47d6b52c
Alec Hall <alecnhall@gmail.com>
2019-07-15 19:42:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ var kittens = [
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  push(name);
-  return kittens;
+  return push(name);
 }
 function destructivelyPrependKitten(name){
 
