eaae1c47e61f990ec402030fde4679f6f2539fdc
Alec Hall <alecnhall@gmail.com>
2019-07-15 19:43:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var kittens = [
 ] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(name){
+function destructivelyAppendKitten(kittens){
   return push("Ralph");
 }
 function destructivelyPrependKitten(name){
